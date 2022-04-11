65d17eca45f5d698528c5119ae73fe475dd0c6a3
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2022-04-11 08:26:21 -0300

    Fix loading state

--- resources/views/livewire/models/create.blade.php
+++ resources/views/livewire/models/create.blade.php
@@ -55,7 +55,7 @@
                         <label for="file" class="block text-sm font-medium text-gray-700">Model File</label>
                         <div class="mt-1">
                             <input type="file" name="file" id="file" wire:model="file">
-                            <div wire:loading wire:target="csvFile">Uploading...</div>
+                            <div wire:loading wire:target="file">Uploading...</div>
                         </div>
                     </div>
                     <div>
@@ -64,7 +64,7 @@
                             <textarea name="metadata" id="metadata" wire:model.defer="metadata" rows="5" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
                         </div>
                     </div>
-                    <button type="submit" class="mt-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <button type="submit" wire:loading.remove class="mt-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Insert
                     </button>
                 </form>
