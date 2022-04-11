7710b97dacc515f3f38a6956cc140601243a8292
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2022-04-11 08:34:57 -0300

    Fix select for 'Brasil' location

--- resources/views/livewire/models/create.blade.php
+++ resources/views/livewire/models/create.blade.php
@@ -14,6 +14,7 @@
                         <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
                         <div class="mt-1">
                             <select id="location" name="location" wire:model="location" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                                <option value="">Select...</option>
                                 <option value="BR">Brasil (BR)</option>
                                 <option value="AC">Acre (AC)</option>
                                 <option value="AL">Alagoas (AL)</option>
